4a4a2d3af2eb0d59c746747c680775c583d8482e
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-12-05 23:08:02 -0500

    frontend starter update

---
+++ frontend/app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+// http://192.168.2.132:5000/foodbanks
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
